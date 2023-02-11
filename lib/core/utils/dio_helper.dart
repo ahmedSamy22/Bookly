172a7dfb1f88aaab520908a5ef12854947e36ca5
ahmedSamy22 <as9593758@gmail.com>
2023-02-11 13:46:21 +0200

    dio helper

---
+++ lib/core/utils/dio_helper.dart
@@ -0,0 +1,13 @@
+import 'package:dio/dio.dart';
+
+class DioHelper {
+  final _baseUrl = 'https://www.googleapis.com/books/v1';
+  final Dio dio;
+
+  DioHelper(this.dio);
+
+  Future<Map<String, dynamic>> getData({required String url}) async {
+    var response = await dio.get('$_baseUrl$url');
+    return response.data;
+  }
+}
